62c1d31a515d3ab1d3e4555c36c7bb6532895b11
Ashley Wong <tofudragon333@gmail.com>
2022-09-23 17:16:06 -0400

    commit tree f0e2826c21c2e8464a7b53e7817faaeb05cf4357

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,4 +2,30 @@ require_relative './node'
 
 class LinkedList
 
+    attr_accessor :head
+
+    def initialize
+        @head = nil
+    end
+
+    def prepend(node)
+        # if head = nil
+        #     head = node
+        # end
+
+        # node = head.next_node
+        node.next_node = head
+        self.head = node
+    end
+
+    def append(node)
+        last_node = head
+        while last_node.next_node
+            last_node = last_node.next_node
+        end
+
+        last_node.push(node)
+
+    end
+
 end
